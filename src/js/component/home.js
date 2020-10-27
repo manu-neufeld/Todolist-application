28ace25b405f6ba7cd40641a2229bcdd825965f9
manu-neufeld <manuela.neufeld@gmail.com>
2020-10-27 14:53:52 +0000

    first part todo llist

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [task, setTask] = useState([]);
+	const [initialValue, setInitialValue] = useState(null);
+	let newTask = event => {
+		let newTask = event.target.value;
+
+		if (event.keyCode == 13) {
+			event.preventDefault();
+			console.log(newTask);
+
+			if (newTask) {
+				setTask(task => [...task, newTask]);
+				setInitialValue("");
+				console.log(task);
+			}
+		}
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<h1>Todo List</h1>
+			<form>
+				<input
+					id="name"
+					type="text"
+					placeholder="Add Task"
+					value={initialValue}
+					onKeyPress={() => {
+						newTask(event);
+					}}
+				/>
+			</form>
+			<ul>
+				{task.map((item, index) => {
+					return <li key={index}>{item}</li>;
+					{
+						console.log(item);
+					}
+				})}
+			</ul>
 		</div>
 	);
 }
