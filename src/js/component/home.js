b516b5b4f727743f573b155032b9de1638dfaf65
manu-neufeld <manuela.neufeld@gmail.com>
2020-10-27 15:44:45 +0000

    imput works

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,18 +5,18 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	let myInput = document.querySelector("#name");
 	const [task, setTask] = useState([]);
 	const [initialValue, setInitialValue] = useState(null);
 	let newTask = event => {
 		let newTask = event.target.value;
-
 		if (event.keyCode == 13) {
 			event.preventDefault();
 			console.log(newTask);
 
 			if (newTask) {
 				setTask(task => [...task, newTask]);
-				setInitialValue("");
+				myInput.value = "";
 				console.log(task);
 			}
 		}
@@ -30,7 +30,6 @@ export function Home() {
 					id="name"
 					type="text"
 					placeholder="Add Task"
-					value={initialValue}
 					onKeyPress={() => {
 						newTask(event);
 					}}
